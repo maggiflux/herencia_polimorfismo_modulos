a8cb4552149b6e81a3ab0d65ca35737328379462
mariela <gila44dri@gmail.com>
2020-01-29 10:14:45 -0300

    desafio herencia polimorfismo y modulos, mariela

---
+++ animal_ejercicio2_parte1.rb
@@ -0,0 +1,51 @@
+#################################
+
+# REVISAR DESAFIO CASI LISTO
+
+#################################
+
+
+module Skills
+
+    module Flyer
+        def fly
+            'I am flyiiinnnggg!'
+        end
+
+        def land
+            'I am tired of flying, I am going to land'
+        end
+    end
+
+    module Swimmer 
+        def swim
+            'I am swimming!'
+        end
+
+        def immerse
+            'glu glub glub glu'
+        end
+    end
+
+    
+    module Walker
+        def walk
+            'I can walk!'
+        end
+    end
+end
+
+    module Feeding
+
+    module Herbivorous 
+        def eat
+            'I can eat plants!'
+        end
+    end
+
+    module Carnivorous
+        def eat
+            'I can eat meat!'
+        end
+    end
+end
